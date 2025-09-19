fdce0b62134c951eaec6f7a4c12ad6b9cad193be
INTEGER <cjs02272002@gmail.com>
2025-09-19 16:24:58 +0900

    20250919_Grade_Integer

--- src/main/java/com/ac/kr/academy/controller/grade/GradeController.java
+++ src/main/java/com/ac/kr/academy/controller/grade/GradeController.java
@@ -3,9 +3,6 @@ package com.ac.kr.academy.controller.grade;
 import com.ac.kr.academy.service.grade.GradeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
